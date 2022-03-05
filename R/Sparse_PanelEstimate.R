7aee1928d1b5ff84eb09927d080134b0aca4f40f
MatteoTiratelli <69040868+MatteoTiratelli@users.noreply.github.com>
2022-03-05 14:24:28 +0000

    Update Sparse_PanelEstimate.R

--- R/Sparse_PanelEstimate.R
+++ R/Sparse_PanelEstimate.R
@@ -1,3 +1,5 @@
+#' Sparse_PanelEstimate 
+#' 
 #' Estimate DiD estimator for Sparse_PanelMatch object with bootstrapped standard errors
 #'
 #' @param data A SparsePanelMatch
@@ -16,15 +18,15 @@ Sparse_PanelEstimate <- function(data, n_iterations = 1000, alpha = 0.05) {
   # Calculate qois
   df1 <- data.table::setDT(output$summary)
   df1$ideological_change <- (df1$outcome - df1$lag_outcome)
-  Estimate <- tibble(coefs = sum(df1$ideological_change*df1$weight, na.rm = TRUE)/length(unique(df1$group)))
+  Estimate <- tibble(coefs = sum(df1$ideological_change*df1$weight)/length(unique(df1$group)))
   Estimate$lead <- 't+0'
-
+  
   if (output$outcome_leads > 0){
-    Estimate <- bind_rows(Estimate, tibble(coefs = sapply(1:output$outcome_leads, function(x) sum(sapply(sapply(1:output$outcome_leads, function (x) paste0('lead_outcome_',x)), function(x) (df1[[x]] - df1$lag_outcome))[,x]*df1$weight, na.rm = TRUE)/length(unique(df1$group))),
+    Estimate <- bind_rows(Estimate, tibble(coefs = sapply(1:output$outcome_leads, function(x) sum(sapply(sapply(1:output$outcome_leads, function (x) paste0('lead_outcome_',x)), function(x) (df1[[x]] - df1$lag_outcome))[,x]*df1$weight)/length(unique(df1$group))),
                                            lead = sapply(1:output$outcome_leads, function (x) paste0('t+',x))))
     Estimate <- Estimate[,c(2,1)]
   }
-
+  
   # Bootstrap SD
   boots <- matrix(NA, nrow = n_iterations, ncol = (output$outcome_leads+1))
   for (k in 1:n_iterations) {
@@ -32,23 +34,21 @@ Sparse_PanelEstimate <- function(data, n_iterations = 1000, alpha = 0.05) {
     units <- sample(clusters, size = length(clusters), replace = T)
     df.bs <- lapply(units, function(x) which(df1[,'unit'] == x)) # creates index of where in main dataset they match each element in 'units'
     d.sub1 <- df1[unlist(df.bs),] # take those indexes to create new dataset
-    boots[k,1] <- sum((d.sub1$outcome - d.sub1$lag_outcome)*d.sub1$weight, na.rm = T)/length(unique(d.sub1$group))
+    boots[k,1] <- sum((d.sub1$outcome - d.sub1$lag_outcome)*d.sub1$weight)/length(unique(d.sub1$group))
     if (output$outcome_leads > 0){
-      boots[k,2:ncol(boots)] <- sapply(1:output$outcome_leads, function(x) sum(sapply(sapply(1:output$outcome_leads, function (x) paste0('lead_outcome_',x)), function(x) (d.sub1[[x]] - d.sub1$lag_outcome))[,x]*d.sub1$weight, na.rm = TRUE)/length(unique(d.sub1$group)))
+      boots[k,2:ncol(boots)] <- sapply(1:output$outcome_leads, function(x) sum(sapply(sapply(1:output$outcome_leads, function (x) paste0('lead_outcome_',x)), function(x) (d.sub1[[x]] - d.sub1$lag_outcome))[,x]*d.sub1$weight)/length(unique(d.sub1$group)))
     }
   }
-
-  Estimate$bootstrap_coefs <- apply(boots, 2, function (x) mean(x, na.rm = T))
-  Estimate$bootstrap_sd <- apply(boots, 2, function (x) sd(x, na.rm = T))
-  Estimate$bootstrap_low <-  apply(boots, 2, quantile, probs = alpha/2, na.rm = T) # percentile confidence interval
-  Estimate$bootstrap_high <-  apply(boots, 2, quantile, probs = (1-(alpha/2)), na.rm = T) # percentile confidence interval
-  Estimate$bootstrap_coefs_BC <- 2*Estimate$coefs - colMeans(boots, na.rm = T) # bias corrected point estimate (Efron & Tibshirani 1993 p138)
+  
+  Estimate$bootstrap_coefs <- apply(boots, 2, function (x) mean(x))
+  Estimate$bootstrap_sd <- apply(boots, 2, function (x) sd(x))
+  Estimate$bootstrap_low <-  apply(boots, 2, quantile, probs = alpha/2) # percentile confidence interval
+  Estimate$bootstrap_high <-  apply(boots, 2, quantile, probs = (1-(alpha/2))) # percentile confidence interval
+  Estimate$bootstrap_coefs_BC <- 2*Estimate$coefs - colMeans(boots) # bias corrected point estimate (Efron & Tibshirani 1993 p138)
   Estimate$bootstrap_low_BC <- apply((2*matrix(nrow = n_iterations, ncol = length(Estimate$coefs), Estimate$coefs, byrow = TRUE) - boots), 2, quantile,
-                                     probs = alpha/2,
-                                     na.rm = T)
+                                     probs = alpha/2)
   Estimate$bootstrap_high_BC <- apply((2*matrix(nrow = n_iterations, ncol = length(Estimate$coefs), Estimate$coefs, byrow = TRUE) - boots), 2, quantile,
-                                      probs = (1-(alpha/2)),
-                                      na.rm = T)# bias corrected confidence interval
+                                      probs = (1-(alpha/2)))# bias corrected confidence interval
   output <- list(summary = Estimate,
                  qoi = output$qoi,
                  covs = output$covs,
