c418c5999e95cb0bba57c1176f4ae6c5ec89564a
MatteoTiratelli <69040868+MatteoTiratelli@users.noreply.github.com>
2022-04-08 15:22:50 +0000

    Update Sparse_PanelMatch.R

--- R/Sparse_PanelMatch.R
+++ R/Sparse_PanelMatch.R
@@ -14,7 +14,7 @@
 #' @param outcome_leads Integer representing the number of outcome leads for which to calculate DiD estimands (each compared to Yt-1).
 #' @param time_window_in_months Integer representing the size of the time window in months within which to search for control observations.
 #' @param match_missing Whether or not to match missing values within treatment history (defaults to TRUE).
-#' @param covs List of covariates to use in matching refinement procedures (should be a list of strings of characters).
+#' @param covs List of covariates to use in matching refinement procedures (should be a list of strings e.g. c('control_1', 'control_2')).
 #' @param qoi The quantity of interest: either 'ATT' (Average Treatment effect on Treated) or 'ATC' (Average Treatment effect on Controls).
 #' @param refinement_method The refinement method used to improve covariate balancing after the exact matching procedure. Can take one of: "none"; "CBPS.weight" (adjusts weights by Covariate Balancing Propensity Scores); "CBPS.match" (reduces the size of each control set to size_match by lowest CBPS); "ps.weight" and "ps.match" do the same for standard propensity scores; "mahalanobis" reduces set size to size_match by Mahalanobis distance. For more details see Imai, Kim & Wang (2018).
 #' @param size_match Integer representing the maximum number of control observations within each refined set. For use with 'CBPS.match', 'ps.match' and 'mahalanobis'. Has no impact otherwise.
